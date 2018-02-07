2d2b754b4a924b51c8409c262cb9ff51d38e3db9
Alex Kaplan <kaplan2539@gmail.com>
2018-02-06 17:29:35 -0800

    fix upload

--- 0030-upload-to-s3.sh
+++ 0030-upload-to-s3.sh
@@ -5,6 +5,9 @@ set -e
 ARCH="${ARCH:-arm64}"
 ROOTFS_FILE="debian_${ARCH}_rootfs_customized.tar.xz"
 
-[[ -z "${AWS_BUCKET}" || -z "${AWS_REGION}" || -z "${AWS_ACCESS_KEY_ID}" || -z "${AWS_SECRET_ACCESS_KEY}" ]] && echo "S3 upload: missing variables -> SKIP" && return
-aws s3 cp --no-progress --acl public-read "${PWD}/${ROOTFS_FILE}" "${AWS_BUCKET}/${ROOTFS_FILE}" 
+AWS_BUCKET="${AWS_BUCKET:?ERROR: AWS_BUCKET not defined}"
+AWS_REGION="${AWS_REGION:?ERROR: AWS_REGION not defined}"
+AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:?ERROR: AWS_ACCESS_KEY_ID not defined}"
+AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:?ERROR AWS_SECRET_ACCESS_KEY not define}"
 
+aws s3 cp --no-progress --acl public-read "${PWD}/${ROOTFS_FILE}" "${AWS_BUCKET}/${ROOTFS_FILE}" 
